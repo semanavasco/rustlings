7693608ff7c93c11cee6dadfa78fd16abacdb718
semanavasco <semanavasco@gmail.com>
2026-02-19 22:32:22 +0100

    done: 12_options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -3,12 +3,16 @@
 // someone eats it all, so no ice cream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day < 22 {
+        Some(5)
+    } else {
+        Some(0)
+    }
 }
 
-fn main() {
-    // You can optionally experiment here.
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -18,7 +22,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let ice_creams = maybe_ice_cream(12);
+        let ice_creams = maybe_ice_cream(12).unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
