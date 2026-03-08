a3ab86984dd84eafa64165dbf585243f99ebad47
semanavasco <semanavasco@gmail.com>
2026-03-08 21:43:44 +0100

    done: 15_traits

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -18,14 +18,11 @@ struct OtherStruct;
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
-fn main() {
-    // You can optionally experiment here.
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
